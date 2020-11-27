bd69b2c321d9d7cec2bc54fe2d5dcb220d124b64
x1ibns <x1ibns@gmail.com>
2020-11-27 00:35:14 -0300

    Primeiro commit da aplicação , contremplando já as criações das tabelas padrões do django.

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app_agenda import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>' ,views.hello ),
+
 ]
--- app_agenda/views.py
+++ app_agenda/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render , HttpResponse
 
 # Create your views here.
-def hello(request,nome):
-    return HttpResponse('<h1> Hello {}</h1>'.format(nome))
