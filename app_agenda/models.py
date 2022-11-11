8890e10cd7f833ba063b0a9289b04ad612ddb70b
x1ibns <x1ibns@gmail.com>
2022-11-11 17:57:33 -0300

    'criado tabela para trablhar com a agenda'

--- app_agenda/models.py
+++ app_agenda/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+"cria tabela de eventos na agenda"
+
+
+class Evento(models.Model):
+    "cria o campo de titulo e descricao"
+    titulo = models.CharField(max_length=100)
+
+    descricao = models.TextField(null=True, blank=True)
+
+    "cria campo de data para o evento"
+    data_evento = models.DateTimeField(verbose_name='Data do evento')
+
+    "data da criacao do evento como parametro de data automatico," \
+    " de forma que o usuario nao pode altera-la "
+    data_criacao = models.DateTimeField(auto_now=True)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def ___str___(self):
+        return self.titulo
--- app_agenda/views.py
+++ app_agenda/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render , HttpResponse
 
 # Create your views here.
+def hello(request):
+    return HttpResponse('<h1>hello</h1>')
