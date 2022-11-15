e01200e63853b1ac9752ff0f30a48a23c8cd5d6b
x1ibns <x1ibns@gmail.com>
2022-11-15 14:36:30 -0300

    'criando pagina de listagem'

--- agenda/urls.py
+++ agenda/urls.py
@@ -20,5 +20,5 @@ from app_agenda import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
-
+    path('agenda/', views.lista_eventos)
 ]
--- app_agenda/views.py
+++ app_agenda/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render, HttpResponse
+from app_agenda.models import Evento
 
 # Create your views here.
 def hello(request):
     return HttpResponse('<h1>hello</h1>')
+
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    data = ({'evento': evento})
+    return render(request, 'agenda.html', data)
